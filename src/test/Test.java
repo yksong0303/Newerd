5a8fe3c6171634a7ef0624259c889bc7468978a7
Rion Park <redfilaruntime@gmail.com>
2020-08-06 11:15:05 +0900

    javascript

--- src/test/Test.java
+++ src/test/Test.java
@@ -2,11 +2,8 @@ package test;
 
 public class Test {
 	public static void main(String[] args) {
-		double d = 1.5;
-		System.out.println(Math.floor(d));
-		System.out.println(Math.round(d));
-		System.out.println(Math.ceil(d));
-		System.out.println(Math.random());
+		String[] strs = new String[1];
+		System.out.println(strs.length);
 	}
 }
 
