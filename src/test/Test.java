d7e5c60ea9459279dcd0203abddd7e88372683cc
Rion Park <redfilaruntime@gmail.com>
2020-08-06 11:42:49 +0900

    test

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,6 +1,10 @@
 package test;
 
 public class Test {
+	int a;
+	void test() {
+		System.out.println(a);
+	}
 	public static void main(String[] args) {
 		String[] strs = new String[1];
 		System.out.println(strs.length);
